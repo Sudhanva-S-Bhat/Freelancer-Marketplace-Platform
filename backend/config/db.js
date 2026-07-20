896e38a24518e5195dfe112fae9d7ea02e1c7358
Sudhanva-S-Bhat <sudhanvasbhat13@gmail.com>
2026-07-20 22:26:38 +0530

    Push 51

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
 
-  const uri = process.env.MONGO_URI;
-  if (!uri) {
-    throw new Error('MONGO_URI is not set in the environment');
-  }
+  const uri = process.env.MONGO_URI || "mongodb://shrinidhis:shrinidhis@ac-gpcbyet-shard-00-00.k9hw0yo.mongodb.net:27017,ac-gpcbyet-shard-00-01.k9hw0yo.mongodb.net:27017,ac-gpcbyet-shard-00-02.k9hw0yo.mongodb.net:27017/?ssl=true&replicaSet=atlas-ckbsj6-shard-0&authSource=admin&appName=Cluster0";
   await mongoose.connect(uri, { dbName: 'freelancer_marketplace' });
   console.log('Connected to MongoDB Atlas');
 }
