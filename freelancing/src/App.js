d6164a38ce151553d8d5acdbbafb1d1a92c6e434
Sudhanva-S-Bhat <sudhanvasbhat13@gmail.com>
2026-07-26 10:11:52 +0530

    Push 82 remove unused react imports from App.js to pass CI build

--- freelancing/src/App.js
+++ freelancing/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
