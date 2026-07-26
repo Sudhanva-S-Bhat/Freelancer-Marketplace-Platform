d3cc72f23f3050844458056c0cf7780f5644d460
Sudhanva-S-Bhat <sudhanvasbhat13@gmail.com>
2026-07-26 09:59:58 +0530

    Push 79 forward styles to Card component in Card.js

--- freelancing/src/components/ui/Card.js
+++ freelancing/src/components/ui/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ children, className = '', hover = false, padding = 'md' }) => {
+const Card = ({ children, className = '', hover = false, padding = 'md', ...props }) => {
   return (
-    <div className={`ui-card p-${padding} ${hover ? 'hover-effect' : ''} ${className}`}>
+    <div className={`ui-card p-${padding} ${hover ? 'hover-effect' : ''} ${className}`} {...props}>
       {children}
     </div>
   );
